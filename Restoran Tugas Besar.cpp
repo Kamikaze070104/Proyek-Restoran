0814c0983a4428ae7df186ce198c1028f0831232
Kamikaze070104 <faizalazzriel@upi.edu>
2022-11-06 22:24:02 +0700

    mengubah tampilan

--- Restoran Tugas Besar.cpp
+++ Restoran Tugas Besar.cpp
@@ -10,19 +10,21 @@ Anggota kelompok :
 4. Nadila putri p 2209757 */
 
 void menu_makanan() {
-    cout << "1. Seblak" << endl;
-    cout << "2. Batagor" << endl;
-    cout << "3. Bakso" << endl;
-    cout << "4. Nasi Goreng" << endl;
-    cout << "5. Bubur Aceh" << endl;
+   
+    cout << "1. Seblak      : Rp 10.000" << endl;
+    cout << "2. Batagor     : Rp 15.000" << endl;
+    cout << "3. Bakso       : Rp 12.000" << endl;
+    cout << "4. Nasi Goreng : Rp 18.000" << endl;
+    cout << "5. Bubur Aceh  : Rp 5.000"  << endl;
 }
 
 void menu_minuman() {
-    cout << "6. Es Kopi Susu" << endl;
-    cout << "7. Brown Sugar Boba" << endl;
-    cout << "8. Matcha Latte" << endl;
-    cout << "9. Kopi original" << endl;
-    cout << "10. Es Krim" << endl;
+    
+    cout << "6. Es Kopi Susu      : Rp 5.000"  << endl;
+    cout << "7. Brown Sugar Boba  : Rp 12.000" << endl;
+    cout << "8. Matcha Latte      : Rp 10.000" << endl;
+    cout << "9. Kopi original     : Rp 4.000"  << endl;
+    cout << "10. Es Krim          : Rp 8.000"  << endl;
 }
 
 int main(){
@@ -77,36 +79,36 @@ int main(){
         cout<<"silahkan pilih menu (1-10): ";
         cin>>menu;
         switch(menu) {
-            case 1 : cout<<"Seblak"<<endl;
-                     cout<<"Rp 10.000"<<endl;
+            case 1 : cout<<"Seblak"<<endl;   
             break;
-            case 2 : cout<<"Batagor"<<endl;
-                     cout<<"Rp 15.000"<<endl;
+            ke
+            case 2 : cout<<"Batagor"<<endl;    
             break;
-            case 3 : cout<<"Nasi Goreng"<<endl;
-                     cout<<"Rp 18.000"<<endl;
+
+            case 3 : cout<<"Bakso"<<endl;   
             break;
-            case 4 : cout<<"Bakso"<<endl;
-                     cout<<"Rp 12.000"<<endl;
+
+            case 4 : cout<<"Nasi Goreng"<<endl;   
             break;
-            case 5 : cout<<"Bubur Aceh"<<endl;
-                     cout<<"Rp 5.000"<<endl;
+
+            case 5 : cout<<"Bubur Aceh"<<endl; 
             break;
-            case 6 : cout<<"Es Kopi Susu"<<endl;
-                     cout<<"Rp 5.000"<<endl;
+
+            case 6 : cout<<"Es Kopi Susu"<<endl; 
             break;
-            case 7 : cout<<"Brown Sugar Boba"<<endl;
-                     cout<<"Rp 12.000"<<endl;
+
+            case 7 : cout<<"Brown Sugar Boba"<<endl;   
             break;
-            case 8 : cout<<"Matcha Latte"<<endl;
-                     cout<<"Rp 10.000"<<endl;
+
+            case 8 : cout<<"Matcha Latte"<<endl; 
             break;
+
             case 9 : cout<<"Kopi Original"<<endl;
-                     cout<<"Rp 4.000"<<endl;
             break;
+
             case 10 : cout<<"Eskrim"<<endl;
-                     cout<<"Rp 8.000"<<endl;
             break;
+
             default : cout<<"tidak ada menu"<<endl;
         }
         
