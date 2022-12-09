adced848ec603166ddb9b0917d14da87bb334295
Kamikaze070104 <faizalazzriel@upi.edu>
2022-12-09 19:01:43 +0700

    sudah fix

--- Restoran Tugas Besar Fix View.cpp
+++ Restoran Tugas Besar Fix View.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <fstream>
-#include <windows.h>
+#include <unistd.h>
 
 using namespace std;
-
 /*Tugas Kelompok 3, kasir restoran
   Anggota kelompok :
    1. Faizal Azzriel Gibar 2205719
    2. Rofi'ul Himam 2206231
    3. Jovanka Alexandro 2206407
    4. Nadila putri Prihanita 2209757 */
-
 string validasi;
-
 // Fungsi Login
-int login() {
+int login() 
+{
   string pass, username;
   string usn_true = "kelompok3";
   string pw_true = "restoran";
@@ -24,18 +22,21 @@ int login() {
   cout << "\t\t\t\t===================================================\n";
   cout << "\t\t\t\t========== Selamat Datang di IZEL RESTO ===========\n";
   cout << "\t\t\t\t===================================================\n\n";
-  for (int i = 2; i >= 0; --i) {
+  for (int i = 2; i >= 0; --i) 
+  {
     cout << "\t\t\t\t                  Silahkan Login                   \n\n";
     cout << "\t\t\t\tUsername: ";
     cin >> username;
     cout << "\t\t\t\tPassword: ";
     cin >> pass;
     system("cls");
-    if (pass == pw_true && username == usn_true) {
+    if (pass == pw_true && username == usn_true) 
+    {
       validasi = "berhasil";
       break;
     }
-    else if (i == 0) {
+    else if (i == 0) 
+    {
       while (counter >= 1)
       {
           cout << "\r\t\t\t\tAnda tidak diperkenankan masuk, coba lagi dalam: " << counter << flush;
@@ -49,33 +50,34 @@ int login() {
     cout << "\t\t\t\t===================================================\n\n";
   }
 }
-
 // Fungsi Pemesanan Makanan
-void pesan_makanan() {
+void pesan_makanan() 
+{
+	ofstream outdata;
   string menu[11] = {"",     "Batagor", "Seblak", "Bakso",  "Rawon", "Soto",
                      "Kopi", "Teh",     "Susu",   "Mojito", "Sprite"};
   int harga[11] = {0,    15000, 10000, 12000, 12500, 17000,
                    6500, 5000,  5000,  8500,  5000};
   string ulang, namaPelanggan;
-  int input = 0, total = 1, output, total_pembayaran = 0, uang, kembalian,
-      metodePembayaran;
+  int input = 0, total = 1, output, total_pembayaran = 0, uang, kembalian, metodePembayaran;
   int pesanan[100], total_harga[100], qty[100];
 
   cout << "\t\t\t\tMasukan nama anda: ";
   cin.ignore();
   getline(cin, namaPelanggan);
   cout << "\t\t\t\tPelanggan dengan atas nama: " << namaPelanggan << "\n";
-  
   cout << "\t\t\t\tDaftar Menu: \n";
-  for (int i = 1; i < 11; i++) {
+  for (int i = 1; i < 11; i++) 
+  {
     cout << "\t\t\t\t" << i << ". " << menu[i] << " : " << harga[i] << "\n";
   }
   cout << "\n";
-
-  do {
+  do 
+  {
     cout << "\t\t\t\tSilahkan pilih menu: ";
     cin >> pesanan[input];
-    if (pesanan[input] > 10) {
+    if (pesanan[input] > 10) 
+    {
       system("cls");
       cout << "\t\t\t\tmenu tidak ditemukan\n";
       return pesan_makanan();
@@ -87,14 +89,16 @@ void pesan_makanan() {
     cout << "\n";
     input++;
     total++;
-  } while (ulang == "y");
+  }
+   while (ulang == "y");
   
   system("cls");
   cout << "\t\t\t\t===================================================\n";
   cout << "\t\t\t\t=================== IZEL RESTO ====================\n";
   cout << "\t\t\t\t===================================================\n\n";
   cout << "\t\t\t\tMenu yang sudah di pesan:\n";
-  for (int i = 0; i < total - 1; i++) {
+  for (int i = 0; i < total - 1; i++) 
+  {
     output = pesanan[i];
     total_harga[i] = harga[output] * qty[i];
 
@@ -102,7 +106,8 @@ void pesan_makanan() {
     cout << "\t\t\t\tJumlah : " << qty[i] << "\n";
     cout << "\t\t\t\tHarga : " << total_harga[i] << "\n\n";
   }
-  for (int i = 0; i < total - 1; i++) {
+  for (int i = 0; i < total - 1; i++) 
+  {
     total_pembayaran += total_harga[i];
   }
   system("pause");
@@ -118,7 +123,8 @@ void pesan_makanan() {
   cout << "\t\t\t\t5.Cash \n";
   cout << "\t\t\t\tPilih metode pembayaran anda: ";
   cin >> metodePembayaran;
-  if (metodePembayaran == 1) {
+  if (metodePembayaran == 1) 
+  {
     // Output nama + total pembayaran + metode pembayaran
     cout << "\n";
     cout << "\t\t\t\t===================================================\n";
@@ -126,7 +132,16 @@ void pesan_makanan() {
     cout << "\t\t\t\tMetode Pembayaran : Dana \n";
     cout << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
     cout << "\t\t\t\t===================================================\n";
-  } else if (metodePembayaran == 2) {
+    outdata.open("struk_bayar.txt", ios::ate);
+    outdata << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+    outdata << "\t\t\t\tNama Pelanggan    : " << namaPelanggan << "\n";
+    outdata << "\t\t\t\tMetode Pembayaran : Dana \n";
+    outdata << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
+    outdata << "\t\t\t\t===================================================\n";  
+  } 
+  else if (metodePembayaran == 2) 
+  {
     // Output nama + total pembayaran + metode pembayaran
     cout << "\n";
     cout << "\t\t\t\t===================================================\n";
@@ -134,7 +149,16 @@ void pesan_makanan() {
     cout << "\t\t\t\tMetode Pembayaran : Ovo \n";
     cout << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
     cout << "\t\t\t\t===================================================\n";
-  } else if (metodePembayaran == 3) {
+    outdata.open("struk_bayar.txt", ios::ate);
+    outdata << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+    outdata << "\t\t\t\tNama Pelanggan    : " << namaPelanggan << "\n";
+    outdata << "\t\t\t\tMetode Pembayaran : Ovo \n";
+    outdata << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+  } 
+  else if (metodePembayaran == 3) 
+  {
     // Output nama + total pembayaran + metode pembayaran
     cout << "\n";
     cout << "\t\t\t\t===================================================\n";
@@ -142,22 +166,41 @@ void pesan_makanan() {
     cout << "\t\t\t\tMetode Pembayaran : LinkAja \n";
     cout << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
     cout << "\t\t\t\t===================================================\n";
-  } else if (metodePembayaran == 4) {
+    outdata.open("struk_bayar.txt", ios::ate);
+    outdata << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+    outdata << "\t\t\t\tNama Pelanggan    : " << namaPelanggan << "\n";
+    outdata << "\t\t\t\tMetode Pembayaran : LinkAja \n";
+    outdata << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+  } 
+  else if (metodePembayaran == 4) 
+  {
     // Output nama + total pembayaran + metode pembayaran
     cout << "\n";
     cout << "\t\t\t\t===================================================\n";
     cout << "\t\t\t\tNama Pelanggan    : " << namaPelanggan << "\n";
-    cout << "\t\t\t\tMetode Pembayaran : GoPay"
-         << "\n";
+    cout << "\t\t\t\tMetode Pembayaran : GoPay";
+    cout << "\n";
     cout << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
     cout << "\t\t\t\t===================================================\n";
-  } else if (metodePembayaran == 5) {
+    outdata.open("struk_bayar.txt", ios::ate);
+    outdata << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+    outdata << "\t\t\t\tNama Pelanggan    : " << namaPelanggan << "\n";
+    outdata << "\t\t\t\tMetode Pembayaran : GoPay \n";
+    outdata << "\t\t\t\tTotal Harga       : Rp" << total_pembayaran << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+  } 
+  else if (metodePembayaran == 5) 
+  {
     // Output nama + total pembayaran + metode pembayaran
     cout << "\n";
     cout << "\t\t\t\tUang Tunai: Rp";
     cin >> uang;
     system("cls");
-    if (uang < total_pembayaran) {
+    if (uang < total_pembayaran) 
+    {
       cout << "\t\t\t\tMaaf, uang tidak cukup.\n";
       return pesan_makanan();
     }
@@ -165,27 +208,41 @@ void pesan_makanan() {
     kembalian = uang - total_pembayaran;
     cout << "\t\t\t\t===================================================\n";
     cout << "\t\t\t\tNama Pelanggan   : " << namaPelanggan << "\n";
-    cout << "\t\t\t\tMetode Pembayaran: Cash"
-         << "\n";
+    cout << "\t\t\t\tMetode Pembayaran: Cash";
+    cout << "\n";
     cout << "\t\t\t\tTotal Harga      : Rp" << total_pembayaran << "\n";
     cout << "\t\t\t\t--------------------------------\n";
     cout << "\t\t\t\tUang Tunai       : Rp" << uang << "\n";
     cout << "\t\t\t\tKembalian        : Rp" << kembalian << "\n";
     cout << "\t\t\t\t===================================================\n";
-  } else {
+    outdata.open("struk_bayar.txt", ios::ate);
+    outdata << "\t\t\t\t===================================================\n";
+    outdata << "\t\t\t\tNama Pelanggan   : " << namaPelanggan << "\n";
+    outdata << "\t\t\t\tMetode Pembayaran: Cash";
+    outdata << "\n";
+    outdata << "\t\t\t\tTotal Harga      : Rp" << total_pembayaran << "\n";
+    outdata << "\t\t\t\t--------------------------------\n";
+    outdata << "\t\t\t\tUang Tunai       : Rp" << uang << "\n";
+    outdata << "\t\t\t\tKembalian        : Rp" << kembalian << "\n";
+    outdata << "\t\t\t\t===================================================\n";
+  } 
+  else 
+  {
     cout << "\t\t\t\tMetode pembayaran tidak tersedia";
     return pesan_makanan();
   }
 }
 
 // Fungsi Cari Menu
-void cari_menu() {
+void cari_menu() 
+{
   string pilihMakanan;
   string hasil;
   string lanjut;
   string makanan[10] = {"seblak", "batagor", "bakso",  "rawon", "kopi",
                         "susu",   "sprite",  "mojito", "teh",   "soto"};
-  do {
+  do 
+  {
     system("cls");
     cout << "\t\t\t\tInput nama Menu yang ingin dicari: ";
     cin.ignore();
@@ -198,28 +255,34 @@ void cari_menu() {
       }
     }
     // Penkondisian hasil pilih makanan dan hasil data yang benar
-    if (pilihMakanan == hasil) {
-      for (int i = 1; i < 10; i++) {
+    if (pilihMakanan == hasil) 
+    {
+      for (int i = 1; i < 10; i++) 
+      {
         cout << "\t\t\t\t" << i << "." << makanan[i] << "\n";
       }
       // Jika benar
       cout << "\t\t\t\tMenu " << hasil << " di temukan"
-           << "\n";
-    } else {
+      cout << "\n";
+    } 
+    else 
+    {
       // Jika salah
       cout << "\t\t\t\tMenu " << pilihMakanan << " tidak di temukan"
-           << "\n";
+      cout << "\n";
     }
     cout << "\t\t\t\tMau cari lagi? (y/t): ";
     cin >> lanjut;
-  } while (lanjut == "y");
+  } 
+  while (lanjut == "y");
 }
-
 // Menu Profil
-void profil() {
+void profil() 
+{
   int a;
   string lagi;
-  do {
+  do 
+  {
     system("cls");
     cout << "\t\t\t\tTentang karyawan Restoran, kelompok 3\n";
     cout << "\t\t\t\t1.Faizal\n";
@@ -228,7 +291,8 @@ void profil() {
     cout << "\t\t\t\t4.Nadila\n";
     cout << "\t\t\t\tSilahkan ingin lihat yang mana(1-4) ";
     cin >> a;
-    switch (a) {
+    switch (a)
+    {
     case 1:
       system("cls");
       cout << "\t\t\t\tNama Lengkap   : Faizal Azzriel Gibar\n";
@@ -258,16 +322,19 @@ void profil() {
     }
     cout << "\t\t\t\tLagi?(y/n) ";
     cin >> lagi;
-  } while (lagi == "y");
+  }
+   while (lagi == "y");
 }
-
 // Program Utama
-int main() {
+int main() 
+{
   int menu;
   string ulang;
   login();
-  if (validasi == "berhasil") {
-    do {
+  if (validasi == "berhasil") 
+  {
+    do 
+    {
       system("cls");
       cout << "\t\t\t\t===================================================\n";
       cout << "\t\t\t\t=================== IZEL RESTO ====================\n";
@@ -280,7 +347,8 @@ int main() {
       cout << "\t\t\t\t5.EXIT\n";
       cout << "\t\t\t\tsilahkan pilih menu(1-5): ";
       cin >> menu;
-      switch (menu) {
+      switch (menu) 
+      {
       case 1:
         system("cls");
         cout << "\t\t\t\t===================================================\n";
@@ -323,7 +391,8 @@ int main() {
       }
       cout << "\t\t\t\tke menu/log out(y/t) ";
       cin >> ulang;
-    } while (ulang == "y");
+    } 
+    while (ulang == "y");
     system("cls");
     cout << "\t\t\t\t<------------( Anda Berhasil Log Out )------------>\n";
     return main();
